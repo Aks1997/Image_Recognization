f46a21d3a27b39beba216de7817e9a4da5c26525
unknown <jkrajput24@gmail.com>
2018-10-14 02:00:53 +0530

    AutoDetecting of Boxes in the image

--- mysite/ocr/forms.py
+++ mysite/ocr/forms.py
@@ -1,7 +1,3 @@
 from django import forms
 class DocumentForm(forms.Form):
     docfile = forms.FileField(label='Select a file',)
-    top_left_width = forms.IntegerField()
-    top_left_height = forms.IntegerField()
-    bottom_right_width = forms.IntegerField()
-    bottom_right_height = forms.IntegerField()
--- mysite/ocr/views.py
+++ mysite/ocr/views.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from django.shortcuts import render, render_to_response
 from PIL import Image, ImageDraw
 import pytesseract
@@ -5,45 +6,89 @@ from .models import document
 from .forms import DocumentForm
 from django.http import HttpResponse
 import time
+import numpy as np
+import cv2
 import os
+
+path = "./ocr/static/ocr/img/" + "temp"+str(int(time.time()))
+
+def getImages(urls):
+    count=1
+    new_images = []
+    image = cv2.imread(urls)
+    grayScale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    sigma = 0.33
+    v = np.median(grayScale)
+    low = int(max(0, (1.0 - sigma) * v))
+    high = int(min(255, (1.0 + sigma) * v))
+    edged = cv2.Canny(grayScale, low, high)
+    (_, cnts, _) = cv2.findContours(edged,
+                                    cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    def detectShape(cnt):
+        shape = 'unknown'
+        peri = cv2.arcLength(c, True)
+        vertices = cv2.approxPolyDP(c, 0.04 * peri, True)
+        if len(vertices) == 3:
+            shape = 'triangle'
+        elif len(vertices) == 4:
+            x, y, width, height = cv2.boundingRect(vertices)
+            aspectRatio = float(width) / height
+            if aspectRatio >= 0.95 and aspectRatio <= 1.05:
+                shape = "square"
+            else:
+                shape = "rectangle"
+        elif len(vertices) == 5:
+            shape = "pentagon"
+        else:
+            shape = "circle"
+        return shape
+    for c in cnts:
+        shape = detectShape(c)
+        if shape =="rectangle":
+            x,y,w,h = cv2.boundingRect(c)
+            if w>20 and h>20:
+                new_img=image[y:y+h,x:x+w]
+                cv2.imwrite(path + str(count) + '.png', new_img)
+                print(path)
+                if str(path) not in new_images:
+                    new_images.append(path + str(count) + '.png')
+                    count +=1
+                print(new_images)
+    return new_images
 
 def list(request):
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
+        # image_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'ocr/static/ocr/img/')
         if form.is_valid():
             # to save the image in the database
             newdoc = document(docfile=request.FILES['docfile'])
-            # newdoc.save()
+            newdoc.save()
 
-            # get the coordinates from the form
-            top_left_width= form.cleaned_data['top_left_width']
-            top_left_height= form.cleaned_data['top_left_height']
-            bottom_right_height = form.cleaned_data['bottom_right_height']
-            bottom_right_width = form.cleaned_data['bottom_right_width']
-
-            # get the all data in the database
-            # documents = document.objects.all()
-            # if using Windows Platform
             if(os.name=='nt'):
                 pytesseract.pytesseract.tesseract_cmd = 'C:/Program Files (x86)/Tesseract-OCR/tesseract'
 
             # To crop the Image
-            image_original = Image.open(newdoc.docfile)
-            image_copy = image_original.copy()
-            temp = image_copy.crop(box=(top_left_width, top_left_height, bottom_right_width, bottom_right_height))
-            data = pytesseract.image_to_string(temp)
+            print(str(newdoc.docfile))
+            new_images_path = []
+            data = []
+            try:
+                new_images_path=getImages('./ocr/static/ocr/' + str(newdoc.docfile))
+            except:
+                data.append('Reupload the Image')
+                form = DocumentForm()
+                return render(request, 'ocr/list.html', {'data':data, 'form':form, 'path':'./ocr/static/ocr/'+ str(newdoc.docfile)})
 
-            # to draw the box on the image_copy
-            ImageDraw.Draw(image_original).rectangle([(top_left_width, top_left_height), (bottom_right_width, bottom_right_height)], fill=None, width=4)
-
-            image_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'ocr/static/ocr/img/')
-            image_original.save(image_path + "temp"+str(int(time.time()))+".png")
-            path = "./ocr/static/ocr/img/" + "temp"+str(int(time.time()))+".png"
-
-            if len(data) == 0:
-                data = "No text!! :<"
+            for i in range(len(new_images_path)):
+                data1 = pytesseract.image_to_string(new_images_path[i])
+                if len(data1)>0:
+                    data.append(data1)
             form = DocumentForm()
-            return render(request, 'ocr/list.html', {'data':data, 'form':form, 'path':path})
+            if(len(data)==0):
+                data.append('No text!! :<')
+            data.reverse()
+            return render(request, 'ocr/list.html', {'data':data, 'form':form, 'path':'./ocr/static/ocr/'+ str(newdoc.docfile)})
 
     # if form is not valid or the request is get
     form = DocumentForm()
